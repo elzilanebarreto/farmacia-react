3006a555abf6cd82d7c459e4e83e4891c44c8df1
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-11-03 15:24:00 -0400

    Fazendo o deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://farmacianest.onrender.com/'
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export const buscar = async (url: string, setDados: Function) => {
